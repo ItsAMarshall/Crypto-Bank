5e801b6de6cea80ec91316e2876e88d04d545ec8
Chris Higley <higleyc@gmail.com>
2014-11-26 15:45:46 -0500

    Initial commit of provied code + initial input parsing on the bank side

---
+++ bank.cpp
@@ -0,0 +1,171 @@
+/**
+	@file bank.cpp
+	@brief Top level bank implementation file
+ */
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+
+#include <string>
+#include <iostream>
+
+void* client_thread(void* arg);
+void* console_thread(void* arg);
+
+int main(int argc, char* argv[])
+{
+	if(argc != 2)
+	{
+		printf("Usage: bank listen-port\n");
+		return -1;
+	}
+	
+	unsigned short ourport = atoi(argv[1]);
+	
+	//socket setup
+	int lsock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(!lsock)
+	{
+		printf("fail to create socket\n");
+		return -1;
+	}
+	
+	//listening address
+	sockaddr_in addr_l;
+	addr_l.sin_family = AF_INET;
+	addr_l.sin_port = htons(ourport);
+	unsigned char* ipaddr = reinterpret_cast<unsigned char*>(&addr_l.sin_addr);
+	ipaddr[0] = 127;
+	ipaddr[1] = 0;
+	ipaddr[2] = 0;
+	ipaddr[3] = 1;
+	if(0 != bind(lsock, reinterpret_cast<sockaddr*>(&addr_l), sizeof(addr_l)))
+	{
+		printf("failed to bind socket\n");
+		return -1;
+	}
+	if(0 != listen(lsock, SOMAXCONN))
+	{
+		printf("failed to listen on socket\n");
+		return -1;
+	}
+	
+	pthread_t cthread;
+	pthread_create(&cthread, NULL, console_thread, NULL);
+	
+	//loop forever accepting new connections
+	while(1)
+	{
+		sockaddr_in unused;
+		socklen_t size = sizeof(unused);
+		int csock = accept(lsock, reinterpret_cast<sockaddr*>(&unused), &size);
+		if(csock < 0)	//bad client, skip it
+			continue;
+			
+		pthread_t thread;
+		pthread_create(&thread, NULL, client_thread, (void*)csock);
+	}
+}
+
+void* client_thread(void* arg)
+{
+	int csock = (int)arg;
+	
+	printf("[bank] client ID #%d connected\n", csock);
+	
+	//input loop
+	int length;
+	char packet[1024];
+	while(1)
+	{
+		//read the packet from the ATM
+		if(sizeof(int) != recv(csock, &length, sizeof(int), 0))
+			break;
+		if(length >= 1024)
+		{
+			printf("packet too long\n");
+			break;
+		}
+		if(length != recv(csock, packet, length, 0))
+		{
+			printf("[bank] fail to read packet\n");
+			break;
+		}
+		
+		//TODO: process packet data
+		
+		//TODO: put new data in packet
+		
+		//send the new packet back to the client
+		if(sizeof(int) != send(csock, &length, sizeof(int), 0))
+		{
+			printf("[bank] fail to send packet length\n");
+			break;
+		}
+		if(length != send(csock, (void*)packet, length, 0))
+		{
+			printf("[bank] fail to send packet\n");
+			break;
+		}
+
+	}
+
+	printf("[bank] client ID #%d disconnected\n", csock);
+
+	close(csock);
+	return NULL;
+}
+
+void* console_thread(void* arg)
+{
+	char buf[80];
+	while(1)
+	{
+		printf("bank> ");
+		fgets(buf, 79, stdin);
+		buf[strlen(buf)-1] = '\0';	//trim off trailing newline
+		
+    std::string input(buf);
+    int first_space = input.find_first_of(' ');
+    if (first_space == input.npos || first_space == input.length() - 1)
+    {
+      std::cerr << "Malformed command\n";
+      continue;
+    }
+    std::string command = input.substr(0, first_space);
+
+    //DEPOSIT
+    if (command == "deposit")
+    {
+      int second_space = input.find_first_of(' ', first_space + 1);
+      if (second_space == input.npos || second_space == input.length() - 1)
+      {
+        std::cerr << "Malformed command\n";
+        continue;
+      }
+      std::string username = input.substr(first_space + 1, second_space - first_space);
+
+      std::string amount_string = input.substr(second_space + 1,
+        input.length() - second_space);
+      //TODO: parse number
+      //TODO: do deposit
+    }
+
+    //BALANCE
+    else if (command == "balance")
+    {
+      std::string username = input.substr(first_space + 1, input.length() - first_space);
+      //TODO: show balance
+    }
+    else
+    {
+      std::cerr << "Unknown command\n";
+    }
+	}
+}
