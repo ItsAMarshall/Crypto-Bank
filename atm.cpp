c372c1302c3380d62a83af4d5237c9667ae19185
Marshall <marshall.shortledge@gmail.com>
2014-11-27 13:11:24 -0500

    Updated atm.  Still working on login process

--- atm.cpp
+++ atm.cpp
@@ -17,7 +17,7 @@
 
 using namespace std;
 
-void withdraw_function(string& input, char packet[]) {
+void withdraw_send(string& input, char packet[]) {
 
 	int sec_space = input.find(" ", 9);
 	string temp = input.substr(9, sec_space);
@@ -30,7 +30,7 @@ void withdraw_function(string& input, char packet[]) {
 	return;
 }
 
-void login_function(string& input, char packet[]) {
+void login_send(string& input, char packet[]) {
 	//Get PIN from Bank
 	//prompt for PIN
 	int sec_space = input.find(" ", 6);
@@ -44,14 +44,14 @@ void login_function(string& input, char packet[]) {
 	return;
 }
 
-void balance_function(string& input, char packet[]) {
+void balance_send(string& input, char packet[]) {
 	printf("Checking Balance\n");
 	for(unsigned int i = 0; i < input.size(); ++i) {
 		packet[i] = input[i];
 	}
 }
 
-void transfer_function(string& input, char packet[]) {
+void transfer_send(string& input, char packet[]) {
 	int space = input.find(" ", 9);
 	string amount = input.substr(9, (space-9));
 	printf("amount %s\n", amount.c_str());
@@ -62,6 +62,40 @@ void transfer_function(string& input, char packet[]) {
 	for(unsigned int i = 0; i < input.size(); ++i) {
 		packet[i] = input[i];
 	}
+}
+
+int send(int sock, int length, char packet[]) {
+	//send the packet through the proxy to the bank
+	if(sizeof(int) != send(sock, &length, sizeof(int), 0))
+	{
+		printf("fail to send packet length\n");
+		return -1;
+	}
+	if(length != send(sock, (void*)packet, length, 0))
+	{
+		printf("fail to send packet\n");
+		return -1;
+	}
+	return 0;
+}
+
+int receive(int sock, int length, char packet[]) {
+	if(sizeof(int) != recv(sock, &length, sizeof(int), 0))
+	{
+		printf("fail to read packet length\n");
+		return -1;
+	}
+	if(length >= 1024)
+	{
+		printf("packet too long\n");
+		return -1;
+	}
+	if(length != recv(sock, packet, length, 0))
+	{
+		printf("fail to read packet\n");
+		return -1;
+	}
+	return 0;
 }
 
 int main(int argc, char* argv[])
@@ -114,60 +148,78 @@ int main(int argc, char* argv[])
 			break;
 		else if(input.find("withdraw ") == 0) {
 			//takes in withdraw [amount]
-			withdraw_function(input, packet);
+			withdraw_send(input, packet);
 		}
 		else if(input.find("login ") == 0) {
 			//takes in login [username]
-			login_function(input, packet);
+			login_send(input, packet);
 
 		}
 		else if(input.find("balance") == 0) {
-			balance_function(input, packet);
+			balance_send(input, packet);
 		}
 		else if(input.find("transfer ") == 0) {
 			//takes in transfer [amount] [username]
-			transfer_function(input, packet);
+			transfer_send(input, packet);
 		}
 		else {
 			printf("invalid input.  Valid operations are:\n \
 login | withdraw | balance | transfer | logout\n");
 		}
 		
-		//send the packet through the proxy to the bank
-		if(sizeof(int) != send(sock, &length, sizeof(int), 0))
-		{
-			printf("fail to send packet length\n");
+		if (send(sock, length, packet) == -1)
 			break;
-		}
-		if(length != send(sock, (void*)packet, length, 0))
-		{
-			printf("fail to send packet\n");
-			break;
-		}
-		
-		//TODO: do something with response packet
-		if(sizeof(int) != recv(sock, &length, sizeof(int), 0))
-		{
-			printf("fail to read packet length\n");
-			break;
-		}
-		if(length >= 1024)
-		{
-			printf("packet too long\n");
-			break;
-		}
-		if(length != recv(sock, packet, length, 0))
-		{
-			printf("fail to read packet\n");
-			break;
-		}
-
-		string str(packet);
-		cout << str << endl;
 
 		for(unsigned int i = 0; i < input.size(); ++i) {
 			packet[i] = '\0';
 		}
+		
+		//TODO: do something with response packet
+		if(receive(sock, length, packet) == -1)
+			break;
+
+		string str(packet);
+		cout << str << endl;
+		
+		/////////
+
+		if(input.find("withdraw ") == 0) {
+			//Successful or not 
+		}
+		else if(input.find("login ") == 0) {
+			//Is user?
+			if( str.find("-1") == 0){
+				printf("User not found.\n");
+			}
+			else if( str.find("1") == 0) {
+				printf("Please enter a PIN\n");
+				string pin;
+				getline(cin, pin); 
+				assert(pin.size() == 4);
+				cout << pin << endl;
+				for(unsigned int i = 0; i < 4; ++i) {
+					packet[i] = pin[i];
+				}
+				if(send(sock, pin.size(), packet) == -1)
+					break;
+				if(receive(sock, length, packet) == -1)
+					break;
+			}
+				//Prompt for pin
+				//Try again
+		}
+		else if(input.find("balance") == 0) {
+			//Return Packet should include balance
+		}
+		else if(input.find("transfer ") == 0) {
+			//Successful or not
+		}
+
+		/////////
+
+		
+
+		
 	}
 	
 	//cleanup
