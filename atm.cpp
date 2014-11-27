b02f23a3bcf0a3728c456114c05226b4aab269d8
Chris Higley <higleyc@gmail.com>
2014-11-27 14:56:08 -0500

    Completed balance functionality

--- atm.cpp
+++ atm.cpp
@@ -1,7 +1,7 @@
 /**
-	@file atm.cpp
-	@brief Top level ATM implementation file
- */
+  @file atm.cpp
+  @brief Top level ATM implementation file
+  */
 #include <string>
 #include <iostream>
 #include <cassert>
@@ -19,218 +19,220 @@ using namespace std;
 
 void withdraw_send(string& input, char packet[]) {
 
-	int sec_space = input.find(" ", 9);
-	string temp = input.substr(9, sec_space);
+  int sec_space = input.find(" ", 9);
+  string temp = input.substr(9, sec_space);
 
-	printf("%s withdrawn.\n", temp.c_str());
+  printf("%s withdrawn.\n", temp.c_str());
 
-	for(unsigned int i = 0; i < input.size(); ++i) {
-		packet[i] = input[i];
-	}
-	return;
+  for(unsigned int i = 0; i < input.size(); ++i) {
+    packet[i] = input[i];
+  }
+  return;
 }
 
 void login_send(string& input, char packet[]) {
-	//Get PIN from Bank
-	//prompt for PIN
-	int sec_space = input.find(" ", 6);
-	string temp = input.substr(6, sec_space);
+  //Get PIN from Bank
+  //prompt for PIN
+  int sec_space = input.find(" ", 6);
+  string temp = input.substr(6, sec_space);
 
-	printf("User %s is logging in.\n", temp.c_str());
+  printf("User %s is logging in.\n", temp.c_str());
 
-	for(unsigned int i = 0; i < input.size(); ++i) {
-		packet[i] = input[i];
-	}
-	return;
+  for(unsigned int i = 0; i < input.size(); ++i) {
+    packet[i] = input[i];
+  }
+  return;
 }
 
 void balance_send(string& input, char packet[]) {
-	printf("Checking Balance\n");
-	for(unsigned int i = 0; i < input.size(); ++i) {
-		packet[i] = input[i];
-	}
+  printf("Checking Balance\n");
+  for(unsigned int i = 0; i < input.size(); ++i) {
+    packet[i] = input[i];
+  }
 }
 
 void transfer_send(string& input, char packet[]) {
-	int space = input.find(" ", 9);
-	string amount = input.substr(9, (space-9));
-	printf("amount %s\n", amount.c_str());
-	space = input.find(" ", 9+amount.size());
-	string destination = input.substr(space+1);
-	printf("Transfering %s to %s.\n", amount.c_str(), destination.c_str());
+  int space = input.find(" ", 9);
+  string amount = input.substr(9, (space-9));
+  printf("amount %s\n", amount.c_str());
+  space = input.find(" ", 9+amount.size());
+  string destination = input.substr(space+1);
+  printf("Transfering %s to %s.\n", amount.c_str(), destination.c_str());
 
-	for(unsigned int i = 0; i < input.size(); ++i) {
-		packet[i] = input[i];
-	}
+  for(unsigned int i = 0; i < input.size(); ++i) {
+    packet[i] = input[i];
+  }
 }
 
 int send(int sock, int length, char packet[]) {
-	//send the packet through the proxy to the bank
-	if(sizeof(int) != send(sock, &length, sizeof(int), 0))
-	{
-		printf("fail to send packet length\n");
-		return -1;
-	}
-	if(length != send(sock, (void*)packet, length, 0))
-	{
-		printf("fail to send packet\n");
-		return -1;
-	}
-	return 0;
+  //send the packet through the proxy to the bank
+  if(sizeof(int) != send(sock, &length, sizeof(int), 0))
+  {
+    printf("fail to send packet length\n");
+    return -1;
+  }
+  if(length != send(sock, (void*)packet, length, 0))
+  {
+    printf("fail to send packet\n");
+    return -1;
+  }
+  return 0;
 }
 
 int receive(int sock, int length, char packet[]) {
-	if(sizeof(int) != recv(sock, &length, sizeof(int), 0))
-	{
-		printf("fail to read packet length\n");
-		return -1;
-	}
-	if(length >= 1024)
-	{
-		printf("packet too long\n");
-		return -1;
-	}
-	if(length != recv(sock, packet, length, 0))
-	{
-		printf("fail to read packet\n");
-		return -1;
-	}
-	return 0;
+  if(sizeof(int) != recv(sock, &length, sizeof(int), 0))
+  {
+    printf("fail to read packet length\n");
+    return -1;
+  }
+  if(length >= 1024)
+  {
+    printf("packet too long\n");
+    return -1;
+  }
+  if(length != recv(sock, packet, length, 0))
+  {
+    printf("fail to read packet\n");
+    return -1;
+  }
+  return 0;
 }
 
 int main(int argc, char* argv[])
 {
-	if(argc != 2)
-	{
-		printf("Usage: atm proxy-port\n");
-		return -1;
-	}
-	
-	//socket setup
-	unsigned short proxport = atoi(argv[1]);
-	int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if(!sock)
-	{
-		printf("fail to create socket\n");
-		return -1;
-	}
-	sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(proxport);
-	unsigned char* ipaddr = reinterpret_cast<unsigned char*>(&addr.sin_addr);
-	ipaddr[0] = 127;
-	ipaddr[1] = 0;
-	ipaddr[2] = 0;
-	ipaddr[3] = 1;
-	if(0 != connect(sock, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)))
-	{
-		printf("fail to connect to proxy\n");
-		return -1;
-	}
-	
-	//input loop
-	char buf[80];
-	string input;
-	while(1)
-	{
-		printf("atm> ");
-		//fgets(buf, 79, stdin);
-		//buf[strlen(buf)-1] = '\0';	//trim off trailing newline
-		getline(cin, input);
-		//TODO: your input parsing code has to put data here
-		char packet[1024];
-		int length = input.size();
-		//char command[8];
-		//strncpy(command, buf, 8);
+  if(argc != 2)
+  {
+    printf("Usage: atm proxy-port\n");
+    return -1;
+  }
 
-		//input parsing
-		if(input.find("logout") == 0)
-			break;
-		else if(input.find("withdraw ") == 0) {
-			//takes in withdraw [amount]
-			withdraw_send(input, packet);
-		}
-		else if(input.find("login ") == 0) {
-			//takes in login [username]
-			login_send(input, packet);
+  //socket setup
+  unsigned short proxport = atoi(argv[1]);
+  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if(!sock)
+  {
+    printf("fail to create socket\n");
+    return -1;
+  }
+  sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(proxport);
+  unsigned char* ipaddr = reinterpret_cast<unsigned char*>(&addr.sin_addr);
+  ipaddr[0] = 127;
+  ipaddr[1] = 0;
+  ipaddr[2] = 0;
+  ipaddr[3] = 1;
+  if(0 != connect(sock, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)))
+  {
+    printf("fail to connect to proxy\n");
+    return -1;
+  }
 
-		}
-		else if(input.find("balance") == 0) {
-			balance_send(input, packet);
-		}
-		else if(input.find("transfer ") == 0) {
-			//takes in transfer [amount] [username]
-			transfer_send(input, packet);
-		}
-		else {
-			printf("invalid input.  Valid operations are:\n \
-login | withdraw | balance | transfer | logout\n");
-		}
-		
-		if (send(sock, length, packet) == -1)
-			break;
+  //input loop
+  char buf[80];
+  string input;
+  bool logged_in = false;
+  while(1)
+  {
+    printf("atm> ");
+    //fgets(buf, 79, stdin);
+    //buf[strlen(buf)-1] = '\0';	//trim off trailing newline
+    getline(cin, input);
+    //TODO: your input parsing code has to put data here
+    char packet[1024];
+    int length = input.size();
+    //char command[8];
+    //strncpy(command, buf, 8);
 
-		for(unsigned int i = 0; i < input.size(); ++i) {
-			packet[i] = '\0';
-		}
-		
-		//TODO: do something with response packet
-		if(receive(sock, length, packet) == -1)
-			break;
+    //input parsing
+    if(input.find("logout") == 0)
+      break;
+    else if(logged_in && input.find("withdraw ") == 0) {
+      //takes in withdraw [amount]
+      withdraw_send(input, packet);
+    }
+    else if(input.find("login ") == 0) {
+      //takes in login [username]
+      login_send(input, packet);
 
-		string str(packet);
-		cout << str << endl;
-		
-		/////////
+    }
+    else if(logged_in && input.find("balance") == 0) {
+      balance_send(input, packet);
+    }
+    else if(logged_in && input.find("transfer ") == 0) {
+      //takes in transfer [amount] [username]
+      transfer_send(input, packet);
+    }
+    else {
+      printf("invalid input.  Valid operations are:\n \
+          login | withdraw | balance | transfer | logout\n");
+      continue;
+    }
 
-		if(input.find("withdraw ") == 0) {
-			//Successful or not 
-		}
-		else if(input.find("login ") == 0) {
-			//Is user?
-			if( str.find("-1") == 0){
-				printf("User not found.\n");
-			}
-			else if( str.find("0") == 0) {
-				printf("Please enter a PIN\n");
-				string pin;
-				getline(cin, pin); 
-				assert(pin.size() == 4);
-				cout << pin << endl;
-				for(unsigned int i = 0; i < 4; ++i) {
-					packet[i] = pin[i];
-				}
-				if(send(sock, pin.size(), packet) == -1)
-					break;
-				if(receive(sock, length, packet) == -1)
-					break;
-				string str(packet);
-				cout << str << endl;
+    if (send(sock, length, packet) == -1)
+      break;
+
+    for(unsigned int i = 0; i < input.size(); ++i) {
+      packet[i] = '\0';
+    }
+
+    if(receive(sock, length, packet) == -1)
+      break;
+
+    string str(packet);
+    cout << str << endl;
+
+    /////////
+
+    if(input.find("withdraw ") == 0) {
+      //Successful or not 
+    }
+    else if(input.find("login ") == 0) {
+      //Is user?
+      if( str.find("-1") == 0){
+        printf("User not found.\n");
+      }
+      else if( str.find("0") == 0) {
+        printf("Please enter a PIN\n");
+        string pin;
+        getline(cin, pin); 
+        assert(pin.size() == 4);
+        cout << pin << endl;
+        for(unsigned int i = 0; i < 4; ++i) {
+          packet[i] = pin[i];
+        }
+        if(send(sock, pin.size(), packet) == -1)
+          break;
+        if(receive(sock, length, packet) == -1)
+          break;
+        string str(packet);
+        cout << str << endl;
         if (str[0] == '0') {
           cout << "You are logged in\n";
+          logged_in = true;
         }
         else {
           cout << "Failed to log in with given PIN\n";
         }
-			}
-				//Prompt for pin
-				//Try again
-		}
-		else if(input.find("balance") == 0) {
-			//Return Packet should include balance
-		}
-		else if(input.find("transfer ") == 0) {
-			//Successful or not
-		}
+      }
+      //Prompt for pin
+      //Try again
+    }
+    else if(input.find("balance") == 0) {
+      cout << "Your balance is $" << str << endl;
+    }
+    else if(input.find("transfer ") == 0) {
+      //Successful or not
+    }
 
-		/////////
+    /////////
 
-		
 
-		
-	}
-	
-	//cleanup
-	close(sock);
-	return 0;
+
+
+  }
+
+  //cleanup
+  close(sock);
+  return 0;
 }
--- bank.cpp
+++ bank.cpp
@@ -1,7 +1,7 @@
 /**
-	@file bank.cpp
-	@brief Top level bank implementation file
- */
+  @file bank.cpp
+  @brief Top level bank implementation file
+  */
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -15,6 +15,7 @@
 #include <string>
 #include <iostream>
 #include <map>
+#include <sstream>
 
 #include "account.h"
 
@@ -26,147 +27,147 @@ std::map<std::string, Account>* accounts;
 
 int main(int argc, char* argv[])
 {
-	if(argc != 2)
-	{
-		printf("Usage: bank listen-port\n");
-		return -1;
-	}
+  if(argc != 2)
+  {
+    printf("Usage: bank listen-port\n");
+    return -1;
+  }
 
   setup_accounts();
-	
-	unsigned short ourport = atoi(argv[1]);
-	
-	//socket setup
-	int lsock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if(!lsock)
-	{
-		printf("fail to create socket\n");
-		return -1;
-	}
-	
-	//listening address
-	sockaddr_in addr_l;
-	addr_l.sin_family = AF_INET;
-	addr_l.sin_port = htons(ourport);
-	unsigned char* ipaddr = reinterpret_cast<unsigned char*>(&addr_l.sin_addr);
-	ipaddr[0] = 127;
-	ipaddr[1] = 0;
-	ipaddr[2] = 0;
-	ipaddr[3] = 1;
-	if(0 != bind(lsock, reinterpret_cast<sockaddr*>(&addr_l), sizeof(addr_l)))
-	{
-		printf("failed to bind socket\n");
-		return -1;
-	}
-	if(0 != listen(lsock, SOMAXCONN))
-	{
-		printf("failed to listen on socket\n");
-		return -1;
-	}
-	
-	pthread_t cthread;
-	pthread_create(&cthread, NULL, console_thread, NULL);
-	
-	//loop forever accepting new connections
-	while(1)
-	{
-		sockaddr_in unused;
-		socklen_t size = sizeof(unused);
-		int csock = accept(lsock, reinterpret_cast<sockaddr*>(&unused), &size);
-		if(csock < 0)	//bad client, skip it
-			continue;
-			
-		pthread_t thread;
-		pthread_create(&thread, NULL, client_thread, (void*)csock);
-	}
+
+  unsigned short ourport = atoi(argv[1]);
+
+  //socket setup
+  int lsock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if(!lsock)
+  {
+    printf("fail to create socket\n");
+    return -1;
+  }
+
+  //listening address
+  sockaddr_in addr_l;
+  addr_l.sin_family = AF_INET;
+  addr_l.sin_port = htons(ourport);
+  unsigned char* ipaddr = reinterpret_cast<unsigned char*>(&addr_l.sin_addr);
+  ipaddr[0] = 127;
+  ipaddr[1] = 0;
+  ipaddr[2] = 0;
+  ipaddr[3] = 1;
+  if(0 != bind(lsock, reinterpret_cast<sockaddr*>(&addr_l), sizeof(addr_l)))
+  {
+    printf("failed to bind socket\n");
+    return -1;
+  }
+  if(0 != listen(lsock, SOMAXCONN))
+  {
+    printf("failed to listen on socket\n");
+    return -1;
+  }
+
+  pthread_t cthread;
+  pthread_create(&cthread, NULL, console_thread, NULL);
+
+  //loop forever accepting new connections
+  while(1)
+  {
+    sockaddr_in unused;
+    socklen_t size = sizeof(unused);
+    int csock = accept(lsock, reinterpret_cast<sockaddr*>(&unused), &size);
+    if(csock < 0)	//bad client, skip it
+      continue;
+
+    pthread_t thread;
+    pthread_create(&thread, NULL, client_thread, (void*)csock);
+  }
 }
 
 void* client_thread(void* arg)
 {
-	int csock = (int)arg;
-	
-	printf("[bank] client ID #%d connected\n", csock);
-	bool withdraw_, balance_, login_, transfer_;
-  bool mid_login = false, validated = false;
-	std::string user_, amount_, pin_;
-  std::map<std::string, Account>::iterator active_account = accounts->end();
-	//input loop
-	int length;
-	char packet[1024];
-	while(1)
-	{
-		withdraw_ = false;
-		balance_ = false;
-		login_ = false;
-		transfer_ = false;
-		//read the packet from the ATM
-		if(sizeof(int) != recv(csock, &length, sizeof(int), 0))
-			break;
-		if(length >= 1024)
-		{
-			printf("packet too long\n");
-			break;
-		}
-		if(length != recv(csock, packet, length, 0))
-		{
-			printf("[bank] fail to read packet\n");
-			break;
-		}
-		
-		//convert packet to string to be parsed
-		std::string str(packet);
+  int csock = (int)arg;
 
-		//determine the command type
+  printf("[bank] client ID #%d connected\n", csock);
+  bool withdraw_, balance_, login_, transfer_;
+  bool mid_login = false, validated = false;
+  std::string user_, amount_, pin_;
+  std::map<std::string, Account>::iterator active_account = accounts->end();
+  //input loop
+  int length;
+  char packet[1024];
+  while(1)
+  {
+    withdraw_ = false;
+    balance_ = false;
+    login_ = false;
+    transfer_ = false;
+    //read the packet from the ATM
+    if(sizeof(int) != recv(csock, &length, sizeof(int), 0))
+      break;
+    if(length >= 1024)
+    {
+      printf("packet too long\n");
+      break;
+    }
+    if(length != recv(csock, packet, length, 0))
+    {
+      printf("[bank] fail to read packet\n");
+      break;
+    }
+
+    //convert packet to string to be parsed
+    std::string str(packet);
+
+    //determine the command type
 
     //if we're mid-login, this is the pin
     if (mid_login) {
       pin_ = str;
     }
-	else if(str.find("withdraw ") == 0) {
-		//takes in withdraw [amount]
-		int sec_space = str.find(" ", 9);
-		std::string temp = str.substr(9, sec_space);
+    else if(str.find("withdraw ") == 0) {
+      //takes in withdraw [amount]
+      int sec_space = str.find(" ", 9);
+      std::string temp = str.substr(9, sec_space);
 
-		//printf("%s withdrawn.\n", temp.c_str());
-		withdraw_ = true;
-		// user_ = somewhere in packet
-	}
-	else if(str.find("login ") == 0) {
-		//takes in login [username] [pin]
-		//Get PIN from Bank
-		//prompt for PIN
-		int sec_space = str.find(" ", 6);
-		user_ = str.substr(6, sec_space);
+      //printf("%s withdrawn.\n", temp.c_str());
+      withdraw_ = true;
+      // user_ = somewhere in packet
+    }
+    else if(str.find("login ") == 0) {
+      //takes in login [username] [pin]
+      //Get PIN from Bank
+      //prompt for PIN
+      int sec_space = str.find(" ", 6);
+      user_ = str.substr(6, sec_space);
 
-		printf("User %s is logging in.\n", user_.c_str());
+      printf("User %s is logging in.\n", user_.c_str());
 
-		login_ = true;
-	}
-	else if(str.find("balance") == 0) {
-		printf("Checking Balance\n");
-		balance_ = true;
+      login_ = true;
+    }
+    else if(str.find("balance") == 0) {
+      printf("Checking Balance\n");
+      balance_ = true;
 
-	}
-	else if(str.find("transfer ") == 0) {
-		//takes in transfer [amount] [username]
-		int space = str.find(" ", 9);
-		std::string amount = str.substr(9, (space-9));
-		printf("amount %s\n", amount.c_str());
-		space = str.find(" ", 9+amount.size());
-		std::string destination = str.substr(space+1);
-		//printf("Transfering %s to %s.\n", amount.c_str(), destination.c_str());
-		
-		transfer_ = true;
-		user_ = destination;
-		amount_ = amount;
-	}
+    }
+    else if(str.find("transfer ") == 0) {
+      //takes in transfer [amount] [username]
+      int space = str.find(" ", 9);
+      std::string amount = str.substr(9, (space-9));
+      printf("amount %s\n", amount.c_str());
+      space = str.find(" ", 9+amount.size());
+      std::string destination = str.substr(space+1);
+      //printf("Transfering %s to %s.\n", amount.c_str(), destination.c_str());
 
-	for(unsigned int i = 0; i < length; ++i) {
-		packet[i] = '\0';
-	}
+      transfer_ = true;
+      user_ = destination;
+      amount_ = amount;
+    }
 
-// -1 for non-existent account, 0 for continue
-	if(login_) {
+    for(unsigned int i = 0; i < 1024; ++i) {
+      packet[i] = '\0';
+    }
+
+    // -1 for non-existent account, 0 for continue
+    if(login_) {
       active_account = accounts->find(user_);
       if (active_account == accounts->end())
       {
@@ -178,7 +179,7 @@ void* client_thread(void* arg)
         packet[0] = '0';
         mid_login = true;
       }
-	}
+    }
     // -1 for bad pin, 0 for success
     else if(mid_login) {
       mid_login = false;
@@ -194,37 +195,48 @@ void* client_thread(void* arg)
         packet[1] = '1';
       }
     }
+    // all other commands require the user to have been authenticated
+    else if(validated) {
+      if (balance_) {
+        // For some reason sprintf isn't working
+        double balance = active_account->second.get_balance();
+        std::stringstream ss;
+        ss << balance;
+        std::string balance_string = ss.str();
+        memcpy(packet, balance_string.c_str(), balance_string.length());
+      }
+    }
 
 
-		//send the new packet back to the client
-		if(sizeof(int) != send(csock, &length, sizeof(int), 0))
-		{
-			printf("[bank] fail to send packet length\n");
-			break;
-		}
-		if(length != send(csock, (void*)packet, length, 0))
-		{
-			printf("[bank] fail to send packet\n");
-			break;
-		}
-		
-	}
+    //send the new packet back to the client
+    if(sizeof(int) != send(csock, &length, sizeof(int), 0))
+    {
+      printf("[bank] fail to send packet length\n");
+      break;
+    }
+    if(length != send(csock, (void*)packet, length, 0))
+    {
+      printf("[bank] fail to send packet\n");
+      break;
+    }
 
-	printf("[bank] client ID #%d disconnected\n", csock);
+  }
 
-	close(csock);
-	return NULL;
+  printf("[bank] client ID #%d disconnected\n", csock);
+
+  close(csock);
+  return NULL;
 }
 
 void* console_thread(void* arg)
 {
-	char buf[80];
-	while(1)
-	{
-		printf("bank> ");
-		fgets(buf, 79, stdin);
-		buf[strlen(buf)-1] = '\0';	//trim off trailing newline
-		
+  char buf[80];
+  while(1)
+  {
+    printf("bank> ");
+    fgets(buf, 79, stdin);
+    buf[strlen(buf)-1] = '\0';	//trim off trailing newline
+
     std::string input(buf);
     int first_space = input.find_first_of(' ');
     if (first_space == input.npos || first_space == input.length() - 1)
@@ -246,7 +258,7 @@ void* console_thread(void* arg)
       std::string username = input.substr(first_space + 1, second_space - first_space - 1);
 
       std::string amount_string = input.substr(second_space + 1,
-        input.length() - second_space);
+          input.length() - second_space);
       double amount = atof(amount_string.c_str());
       if (amount <= 0)
       {
@@ -268,7 +280,7 @@ void* console_thread(void* arg)
     else if (command == "balance")
     {
       std::string username = input.substr(first_space + 1, input.length() - first_space);
-      
+
       std::map<std::string, Account>::iterator it;
       it = accounts->find(username);
       if (it == accounts->end())
@@ -283,7 +295,7 @@ void* console_thread(void* arg)
     {
       std::cerr << "Unknown command\n";
     }
-	}
+  }
 }
 
 void setup_accounts()
